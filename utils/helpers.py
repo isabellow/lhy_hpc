eaa1e7646ef0c1588681f914ef07432c9169283d
isabellow <ilow@stanford.edu>
2023-10-20 19:35:38 -0400

    starter code to look at chronic stim events

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import butter, bessel, filtfilt
 
 def nan_interp(y):
     def find(x):
@@ -6,3 +7,37 @@ def nan_interp(y):
     nans = np.isnan(y)
     y[nans] = np.interp(find(nans),find(~nans),y[~nans])
     return y
+
+
+def bandpass(x, lowcut, highcut, fs, order=5, axis=-1, kind='butter'):
+    """
+    Modified slightly from AHW.
+
+    Parameters
+    ----------
+    x : ndarray
+        1d time series data
+    lowcut : float
+        Defines lower frequency cutoff (e.g. in Hz)
+    highcut : float
+        Defines upper frequency cutoff (e.g. in Hz)
+    fs : float
+        Sampling frequency (e.g. in Hz)
+    order : int
+        Filter order parameter
+    kind : str
+        Specifies the kind of filter
+        butter for butterworth; bessel for bessel
+    axis : int
+        Axis along which to bandpass filter data
+    """
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    if kind == "butter":
+        b, a = butter(order, [low, high], btype="band")
+    elif kind == "bessel":
+        b, a = bessel(order, [low, high], btype="bandpass")
+    else:
+        raise ValueError("Filter kind not recognized.")
+    return filtfilt(b, a, x, axis=axis)
