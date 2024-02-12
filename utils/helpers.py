d14167ec67281790cbc75995948a471d0f1b01f3
isabellow <ilow@stanford.edu>
2024-02-12 11:47:10 -0500

    minor updates

--- utils/helpers.py
+++ utils/helpers.py
@@ -9,6 +9,7 @@ def nan_interp(y):
     return y
 
 
+''' Filtering '''
 def bandpass(x, lowcut, highcut, fs, order=5, axis=-1, kind='butter'):
     """
     Modified slightly from AHW.
@@ -38,6 +39,36 @@ def bandpass(x, lowcut, highcut, fs, order=5, axis=-1, kind='butter'):
         b, a = butter(order, [low, high], btype="band")
     elif kind == "bessel":
         b, a = bessel(order, [low, high], btype="bandpass")
+    else:
+        raise ValueError("Filter kind not recognized.")
+    return filtfilt(b, a, x, axis=axis)
+
+def highpass(x, highcut, fs, order=5, axis=-1, kind='butter'):
+    """
+    Modified slightly from AHW.
+
+    Parameters
+    ----------
+    x : ndarray
+        1d time series data
+    highcut : float
+        Defines upper frequency cutoff (e.g. in Hz)
+    fs : float
+        Sampling frequency (e.g. in Hz)
+    order : int
+        Filter order parameter
+    kind : str
+        Specifies the kind of filter
+        butter for butterworth; bessel for bessel
+    axis : int
+        Axis along which to bandpass filter data
+    """
+    nyq = 0.5 * fs
+    high = highcut / nyq
+    if kind == "butter":
+        b, a = butter(order, high, btype="high")
+    elif kind == "bessel":
+        b, a = bessel(order, high, btype="highpass")
     else:
         raise ValueError("Filter kind not recognized.")
     return filtfilt(b, a, x, axis=axis)
