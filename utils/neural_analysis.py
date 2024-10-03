31bfb37e0f973d44b819660f0b21852064b238f0
isabellow <ilow1991@gmail.com>
2024-10-03 11:20:19 -0400

    prelim place coding analysis

--- utils/neural_analysis.py
+++ utils/neural_analysis.py
@@ -48,3 +48,59 @@ def tuning_curve_1d(x, Y, dt, b, smooth=True, l=2, SEM=False):
         return firing_rate, centers, spike_sem
     else:
         return firing_rate, centers
+
+
+def tuning_curve_2d(X, Y, dt, b_lims, b):
+    '''
+    Params
+    ------
+    X : ndarray
+        variable of interest by observation; shape (n_obs, 2)
+    Y : ndarray
+        spikes per observation; shape (n_cells, n_obs)
+    dt : int
+        time per observation in seconds
+    b_lims : tuple of floats
+        min/max values considered
+    b : int
+        bin size
+    
+    Returns
+    -------
+    firing_rate : ndarray
+        average firing rate per 2d bin
+        shape (n_bins, n_bins)
+    centers : ndarray
+        center of each bin
+        shape (2, n_bins)
+    bin_idx : ndarray
+        index for each firing rate
+        shape (2, n_bins)
+    '''
+    edges = np.arange(b_lims[0], b_lims[1] + b, b)
+    centers = (edges[:-1] + edges[1:])/2
+    
+    x1_idx = np.digitize(X[:, 0], edges)
+    if np.max(X[:, 0]) == edges[-1]:
+        x1_idx[x1_idx==np.max(x1_idx)] = np.max(x1_idx) - 1
+    x2_idx = np.digitize(X[:, 1], edges)
+    if np.max(X[:, 1]) == edges[-1]:
+        x2_idx[x2_idx==np.max(x2_idx)] = np.max(x2_idx) - 1
+    unique_bdx_x1 = np.unique(x1_idx)
+    unique_bdx_x2 = np.unique(x2_idx)
+    
+    n_bins = edges.shape[0] + 1
+    n_cells = Y.shape[0]
+    
+    firing_rate = np.zeros((n_cells, n_bins, n_bins))
+    for i in unique_bdx_x1:
+        for j in unique_bdx_x2:
+            pos_idx = (x1_idx == i) & (x2_idx == j)
+            spike_ct = np.sum(Y[:, pos_idx], axis=1)
+            occupancy = dt * np.sum(pos_idx)
+            if occupancy < 0.1:
+                firing_rate[:, i, j] = np.NaN
+            else:
+                firing_rate[:, i, j] = spike_ct / occupancy
+    
+    return firing_rate, centers
