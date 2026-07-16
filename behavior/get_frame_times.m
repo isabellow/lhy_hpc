7d6d6009fab9afb775c30565796e41b554d44db4
unknown <ilow1991@gmail.com>
2026-07-16 14:09:07 -0400

    prelim code for bombcell

--- behavior/get_frame_times.m
+++ behavior/get_frame_times.m
@@ -2,11 +2,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Set file paths
 addpath(genpath('C:\Users\ilow1\Documents\code\lhy_hpc\utils\')) % functions to read Intan files
-bird_id = 'AMB154';
-session_date = '241114';
-session_root = ['Z:\Isabel\data\hpc_implants\', bird_id, '\'];
-session_dir = [bird_id, '_', session_date '\'];
-ephys_dir = 'AMB154_241114_110440\';
+bird_id = 'LMN88';
+session_date = '260714';
+% session_root = ['Z:\Isabel\data\hpc_implants\', bird_id, '\'];
+session_root = ['Z:\Isabel\data\lhy_implants\', bird_id, '\'];
+session_dir = [bird_id, '_', session_date '\rec_2\'];
+ephys_dir = 'LMN88_260714_144143\';
 
 data_dir = fullfile(fileparts(session_root), [session_dir, ephys_dir]);
 
@@ -22,13 +23,13 @@ mkdir(save_dir)
 % frames to keep
 frame_rate = 50;
 start_frame_idx = 0*frame_rate + 1; % first frame to keep
-end_frame_idx = ((2*60 + 20)*60 + 19)*50; % number of frames to keep
-% end_frame_idx = 466689;
+% end_frame_idx = ((2*60 + 20)*60 + 19)*50; % number of frames to keep
+end_frame_idx = 180468;
 
 %% load the frame times from Intan
 % Get the digital input (frame times on dig in ch1)
 % [dig_data, h] = readIntanDig(data_dir, duration_t, start_t); % read in select frame times
-[dig_data, h] = readIntanDig(data_dir); % read in all the frame times
+[dig_data, header] = readIntanDig(data_dir); % read in all the frame times
 frame_in = dig_data(1, :);
 n_samples = length(frame_in);
 
@@ -43,15 +44,16 @@ end
 n_total_frames = length(frame_starts);
 
 % check dt
-frame_dt = (diff(frame_starts)) / h.sample_rate;
+frame_dt = (diff(frame_starts)) / header.sample_rate;
 disp(unique(round(frame_dt, 2)))
 
 %% Trim the frame times and convert to seconds
-frame_starts = frame_starts(start_frame_idx:end_frame_idx);
-frame_ends = frame_ends(start_frame_idx:end_frame_idx);
-framet = frame_starts / h.sample_rate;
+frame_starts = frame_starts(start_frame_idx+1:end_frame_idx);
+frame_ends = frame_ends(start_frame_idx+1:end_frame_idx);
+framet = frame_starts / header.sample_rate;
 
 %% save to a numpy array
 writeNPY(framet, fullfile(save_dir, save_file_name));
+save(fullfile(save_dir, 'intan_info.mat'), 'header')
 
 
